ede3f82f39c8c7940726aa9af6197c8a00226440
HaydenSchalow <hayden_schalow@mines.edu>
2025-11-11 22:16:04 -0700

    Update mkdirSplitpath.c

--- mkdirSplitpath.c
+++ mkdirSplitpath.c
@@ -3,6 +3,65 @@
 extern struct NODE* root;
 extern struct NODE* cwd;
 
+void mkdir(char pathName[]) {
+    if (strcmp(pathName, "/") == 0 || strlen(pathName) == 0) {
+        printf("MKDIR ERROR: no path provided\n");
+        return;
+    }
+
+    struct NODE* current = (pathName[0] == '/') ? root : cwd;
+    char temp[128];
+    strcpy(temp, pathName);
+    char* token = strtok(temp, "/");
+
+    while (token != NULL) {
+        struct NODE* child = current->childPtr;
+        struct NODE* found = NULL;
+
+        // Search for existing directory
+        while (child != NULL) {
+            if (strcmp(child->name, token) == 0 && child->fileType == 'D') {
+                found = child;
+                break;
+            }
+            child = child->siblingPtr;
+        }
+
+        if (found) {
+            current = found; // Move deeper
+        } else {
+            // Create new directory
+            struct NODE* newNode = (struct NODE*)malloc(sizeof(struct NODE));
+            strcpy(newNode->name, token);
+            newNode->fileType = 'D';
+            newNode->parentPtr = current;
+            newNode->childPtr = NULL;
+            newNode->siblingPtr = NULL;
+
+            // Insert into child list
+            if (current->childPtr == NULL) {
+                current->childPtr = newNode;
+            } else {
+                struct NODE* last = current->childPtr;
+                while (last->siblingPtr != NULL) {
+                    last = last->siblingPtr;
+                }
+                last->siblingPtr = newNode;
+            }
+
+            printf("MKDIR: created directory %s\n", token);
+            current = newNode;
+        }
+
+        token = strtok(NULL, "/");
+    }
+
+    printf("MKDIR SUCCESS: full path %s created\n", pathName);
+}
+
+
+
+/*
 //make directory
 void mkdir(char pathName[]) {
     if (strcmp(pathName, "/") == 0 || strlen(pathName) == 0) {
@@ -49,6 +108,7 @@ while (child != NULL) {
 
     printf("MKDIR SUCCESS: node %s successfully created\n", baseName);
 }
+*/
 
 //handles tokenizing and absolute/relative pathing options
 struct NODE* splitPath(char* pathName, char* baseName, char* dirName) {
